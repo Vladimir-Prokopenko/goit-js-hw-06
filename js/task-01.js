25c18a400cac16700177606f7cb50452f194964e
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-01-20 16:04:58 +0200

    task 1.2 ok

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
 const numberOfCategories = document.querySelector("#categories");
 console.log("Number of categories:", numberOfCategories.children.length);
+
+const child = numberOfCategories.children;
+
+[...child].forEach((categorie) => {
+  console.log("Category:", categorie.firstElementChild.textContent);
+  console.log("Elements:", categorie.lastElementChild.children.length);
+});
