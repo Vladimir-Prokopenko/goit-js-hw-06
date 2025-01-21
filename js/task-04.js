6743f07d8b6e623052074436658aa38b702030d1
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-01-21 18:40:57 +0200

    task4 start

---
+++ js/task-04.js
@@ -0,0 +1,6 @@
+const counterValue = document.querySelector("#value");
+
+const buttonDecrement = document.querySelector("action-decrement");
+console.log("buttonDecrement:", buttonDecrement);
+
+// console.log("counterValue:", counterValue.textContent);
