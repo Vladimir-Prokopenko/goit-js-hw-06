5f04d283628eb77bf77fc38039452a87e4514101
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-01-23 14:54:31 +0200

    task08ok

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const loginFormRef = document.querySelector(".login-form");
+const inputEemailRef = loginFormRef.firstElementChild.lastElementChild;
+const inputPasswordRef = loginFormRef.querySelectorAll("input")[1];
+const buttonTypeSubmitRef = loginFormRef.querySelector("button");
 
+loginFormRef.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (!mail || !password) {
+    alert("Все поля должны быть заполнены!");
+  } else {
+    const formData = {
+      mail,
+      password,
+    };
+    console.log(formData);
+
+    loginFormRef.reset();
+  }
+}
