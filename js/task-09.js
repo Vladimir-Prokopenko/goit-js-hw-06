d3df28114d6b2113104ec08e1f3e9642797f7ead
Владимир Прокопенко <profcompanysumy@gmail.com>
2025-01-23 15:21:47 +0200

    task09ok

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyWidgetRef = document.querySelector("body");
+
+const changeColorButtonRef = document.querySelector(".change-color");
+
+const spanButtonColorRef = document.querySelector(".color");
+
+changeColorButtonRef.addEventListener("click", onClickFunctionColor);
+
+function onClickFunctionColor(event) {
+  const bodyColor = getRandomHexColor();
+  spanButtonColorRef.textContent = bodyColor;
+  bodyWidgetRef.style.backgroundColor = bodyColor;
+}
